ab08ed240136709ef0394753e79c1e9cab427ac7
Edlenko Sergey <zadroshy@gmail.com>
2022-09-21 12:31:46 +0300

    1 task

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        DecryptTrivial decryptTrivial = new DecryptTrivial();
+        String decryptedString = "блюншж явфвн, бйёпнёж яиэбёйёнляёф! оплизкриоь о еэбэфвж," + "злабэ кэ яштлбв квжнлккэь овпщ блидкэ яшбэяэпщ кв плищзл зиэоо" + "(зиэооёсёзэуёь) ё кв мнлопл фёоил (нванвооёь). кэ яштлбв крдкл ялеянэцэпщ x," + "y, width, height ё зиэоо люкэнрдвкклал кэ зэнпёкзв лючвзпэ (ёиё зллнбёкэпш" + "плфвз злкпрнля ъплал лючвзпэ, э-иь овайвкпэуёь).";
+
+        decryptTrivial.substitutionDecrypt(decryptedString);
+    }
+}
