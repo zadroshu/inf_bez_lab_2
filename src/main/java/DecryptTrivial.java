dff79bd6826bc67717dfa15a02a85740e4697e88
Edlenko Sergey <zadroshy@gmail.com>
2022-09-22 20:21:04 +0300

    First task

--- src/main/java/DecryptTrivial.java
+++ src/main/java/DecryptTrivial.java
@@ -1,29 +1,27 @@
 public class DecryptTrivial {
-    public String decryptedString = "блюншж явфвн, бйёпнёж яиэбёйёнляёф! оплизкриоь о еэбэфвж," + "злабэ кэ яштлбв квжнлккэь овпщ блидкэ яшбэяэпщ кв плищзл зиэоо" + "(зиэооёсёзэуёь) ё кв мнлопл фёоил (нванвооёь). кэ яштлбв крдкл ялеянэцэпщ x," + "y, width, height ё зиэоо люкэнрдвкклал кэ зэнпёкзв лючвзпэ (ёиё зллнбёкэпш" + "плфвз злкпрнля ъплал лючвзпэ, э-иь овайвкпэуёь).";
-
-    public void substitutionDecrypt(String decryptedString ) {
-        int shift = 0;
-        String alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
-        while (shift <= alphabet.length() - 1) {
-            String rez = "";
-            for (char c: decryptedString.toCharArray()) {
-                if (c != ' ') {
-                    if (!alphabet.contains(String.valueOf(c))){
-                        rez = rez.concat(String.valueOf(c));
-                    } else {
-                        int i = alphabet.indexOf(c);
-                        i = (i + shift) % alphabet.length();
-                        if (i < 0) {
-                            i = i + alphabet.length();
-                        }
-                        rez = rez + alphabet.charAt(i);
-                    }
-                } else {
-                    rez = rez + " ";
-                }
+  public void substitutionDecrypt(String decryptedString) {
+    int shift = 0;
+    String alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+    while (shift <= alphabet.length() - 1) {
+      String rez = "";
+      for (char c : decryptedString.toCharArray()) {
+        if (c != ' ') {
+          if (!alphabet.contains(String.valueOf(c))) {
+            rez = rez.concat(String.valueOf(c));
+          } else {
+            int i = alphabet.indexOf(c);
+            i = (i - shift + alphabet.length()) % alphabet.length();
+            if (i < 0) {
+              i = i + alphabet.length();
             }
-            System.out.println(rez);
-            shift++;
+            rez += alphabet.charAt(i);
+          }
+        } else {
+          rez = rez + " ";
         }
+      }
+      System.out.println(shift + " " + rez);
+      shift++;
     }
+  }
 }
